a4ff5cac5cf5b6a32aa6c840d8dd9fc9b0eff7ad
Yugandhar <yugandharn313@gmail.com>
2023-02-17 14:17:19 +0530

    Set up file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="yugandharn313@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
