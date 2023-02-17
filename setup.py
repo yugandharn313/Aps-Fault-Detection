9929015d3b1511b68cbef4b30f316702a615dc89
Yugandhar <yugandharn313@gmail.com>
2023-02-18 00:25:45 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import find_packages,setup
 
-def get_requirements():
-    pass
+from typing import List
+
+HYPHEN_E_DOT="-e."
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list=requirement_file.readlines()
+        requirement_list=[requirement_name.replace("/n","") for requirement_name in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+
+    return requirement_list
 setup(
     name="sensor",
     version="0.0.1",
