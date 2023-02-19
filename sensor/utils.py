eab782b81330ced0ee460edc5fd8222b9ee953a2
Yugandhar <yugandharn313@gmail.com>
2023-02-19 14:23:22 +0530

    Data Validation Completed

---
+++ sensor/utils.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sensor.logger import logging
+from sensor.exception import SensorException
+from sensor.config import mongo_client
+import yaml
+import os,sys 
+def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
+    
+    try:
+        logging.info(f"Reading data from database :{database_name} and collection :{collection_name}")
+        df=pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        logging.info(f"found columns:{df.columns}")
+        if "_id" in df.columns:
+            df=df.drop("_id",axis=1)
+        logging.info(f"Row and columns in df: {df.shape}")
+        return df
+    except Exception as e:
+        raise SensorException(e,sys)
+
+def write_yaml_file(file_path,data:dict):
+        try:
+            file_dir=os.path.dirname(file_path)
+            os.makedirs(file_dir,exist_ok=True)
+            with open(file_path,"w") as file_writter:
+                yaml.dump(data,file_writter)
+        except Exception as e:
+            raise SensorException(e, sys)
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise e
