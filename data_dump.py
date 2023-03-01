96319dd9873c4c0ba47ad989806b80f5464fc6d0
Yugandhar <yugandharn313@gmail.com>
2023-03-01 23:18:16 +0530

    Project Completed

--- data_dump.py
+++ data_dump.py
@@ -2,7 +2,7 @@ import pymongo
 import pandas as pd
 import json
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017")
+client = pymongo.MongoClient("mongodb+srv://Yugandhar:Yesh1456@cluster.0lweolz.mongodb.net/?retryWrites=true&w=majority")
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
